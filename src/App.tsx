2c7b1e36d1ddf82f40a03bfdbfa6d46471030d50
Natalia Panchuk <Foreverdanda@ukr.net>
2026-03-13 18:28:05 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
 
-// import { getAll, get5First, getRed } from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { Good } from './types/Good';
+
+export const App: React.FC = () => {
+  const [goods, setGoods] = useState<Good[]>([]);
+
+  return (
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
+      <button
+        type="button"
+        data-cy="all-button"
+        onClick={() => {
+          getAll().then(result => setGoods(result));
+        }}
+      >
+        Load all goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="first-five-button"
+        onClick={() => {
+          get5First().then(result => setGoods(result));
+        }}
+      >
+        Load 5 first goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="red-button"
+        onClick={() => {
+          getRedGoods().then(result => setGoods(result));
+        }}
+      >
+        Load red goods
+      </button>
+
+      <GoodsList goods={goods} />
+    </div>
+  );
+};
+
 // or
 // import * as goodsAPI from './api/goods';
 
-export const App: React.FC = () => (
+/*export const App: React.FC = () => (
   <div className="App">
     <h1>Dynamic list of Goods</h1>
 
@@ -24,4 +67,4 @@ export const App: React.FC = () => (
 
     <GoodsList goods={[]} />
   </div>
-);
+);*/
